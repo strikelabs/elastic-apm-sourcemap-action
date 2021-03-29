286b56e35a063dead9a240a5cf6dcb4d13a01c1a
finleygn <finleygarton12@gmail.com>
2021-03-29 15:42:56 +0100

    added url join util

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const url = require('url');
 const path = require('path');
 const core = require('@actions/core');
 const glob = require('@actions/glob');
@@ -7,19 +8,26 @@ const httpm = require('@actions/http-client');
 const http = new httpm.HttpClient();
 
 /**
+ * @param {string} a 
+ * @param {string} b 
+ */
+function joinURLPath(a, b) {
+  return a.replace(/\/$/, "") + "/" + b.replace(/^\//, "");
+}
+
+/**
  * Upload a single sourcemap to elastic
  * @param {string} map 
  */
 async function upload(sourcemap) {
-  const url = sourcemap
-    .split(core.getInput('sourcemap_build_directory'))[1]
-    .replace('.map', ''); // TODO: Not everything ends in .map '~'
+  const sourcemap_path = sourcemap.split(path.join(core.getInput('sourcemap_build_directory')))[1];
+  const source_path = sourcemap_path.replace('.map', ''); // TODO: Not everything ends in .map '~'
 
   const response = await http.post(core.getInput('elastic_node'), {
     sourcemap: fs.createReadStream(sourcemap),
     service_version: core.getInput('service_version'),
     service_name: core.getInput('service_name'),
-    bundle_filepath: path.join(core.getInput('bundle_url'), url),
+    bundle_filepath: joinURLPath(core.getInput('bundle_url'), source_path),
   }, {
     Authorization: `Bearer ${core.getInput("token")}`
   });
