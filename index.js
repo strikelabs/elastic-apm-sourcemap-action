bca035060d9223b51e34660812d74f68eb438551
finleygn <finleygarton12@gmail.com>
2021-03-30 10:34:52 +0100

    move to node fetch

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const core = require('@actions/core');
 const glob = require('@actions/glob');
-const httpm = require('@actions/http-client');
-
-const http = new httpm.HttpClient();
+const fetch = require('node-fetch');
+const FormData = require('form-data');
 
 /**
  * @param {string} a 
@@ -22,23 +21,23 @@ async function upload(sourcemap) {
   const sourcemap_path = sourcemap.split(path.join(core.getInput('sourcemap_build_directory')))[1];
   const source_path = sourcemap_path.replace('.map', ''); // TODO: Not everything ends in .map '~'
 
-  const response = await http.post(
+  const body = new FormData();
+
+  body.append('sourcemap',       fs.createReadStream(sourcemap));
+  body.append('service_version', core.getInput('service_version'));
+  body.append('service_name',    core.getInput('service_name'));
+  body.append('bundle_filepath', joinURLPath(core.getInput('bundle_url'), source_path));
+
+  return fetch(
     joinURLPath(core.getInput('apm_node'), "/assets/v1/sourcemaps"),
-    {
-      sourcemap: fs.createReadStream(sourcemap),
-      service_version: core.getInput('service_version'),
-      service_name: core.getInput('service_name'),
-      bundle_filepath: joinURLPath(core.getInput('bundle_url'), source_path),
-    },
-    {
-      Authorization: `Bearer ${core.getInput("token")}`
+    { 
+      method: "POST",
+      body, 
+      headers: {
+        Authorization: `Bearer ${core.getInput("token")}`
+      }
     }
   );
-
-  if(response.message.statusCode >= 400) {
-    const body = await response.readBody();
-    throw Error(`Failed to upload sourcemap ${sourcemap}`, response.statusCode, body);
-  }
 }
 
 async function run() {
